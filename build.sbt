0ffb1d384e5c1951123cbb05c9094b5df9745269
Bibin Nahas <bbnnaz@gmail.com>
2020-08-22 22:47:00 +0530

    Sample scala codes for learning

--- build.sbt
+++ build.sbt
@@ -2,15 +2,16 @@ name := "HelloSparkScala"
 
 version := "0.1"
 
-scalaVersion := "2.12.10"
+scalaVersion := "2.11.12"
 
-val sparkVersion = "3.0.0"
+val sparkVersion = "2.4.0"
 
 libraryDependencies ++= Seq(
 
   "org.apache.spark" %% "spark-hive" % sparkVersion,
   "org.apache.spark" %% "spark-sql" % sparkVersion,
   "org.apache.spark" %% "spark-core" % sparkVersion,
-  "org.apache.spark" %% "spark-catalyst" % sparkVersion
+  "org.apache.spark" %% "spark-catalyst" % sparkVersion,
+  "org.apache.spark" %% "spark-mllib" % "2.2.1"
 
 )
--- src/main/scala/com/bbn/sample/ML.scala
+++ src/main/scala/com/bbn/sample/ML.scala
@@ -1,10 +1,20 @@
 package com.bbn.sample
 
 import java.nio.charset.CodingErrorAction
+
+import org.apache.spark.sql.SparkSession
+
 import scala.io.{Codec, Source}
 
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+import scala.io.Source
+import java.nio.charset.CodingErrorAction
+import scala.io.Codec
+import org.apache.spark.ml.recommendation._
+
 object ML {
-  def loadMovies:Map[Int, String] = {
+  def loadMovies: Map[Int, String] = {
     implicit val codec = Codec("UTF-8")
     codec.onMalformedInput(CodingErrorAction.REPLACE)
     codec.onUnmappableCharacter(CodingErrorAction.REPLACE)
@@ -23,17 +33,40 @@ object ML {
   case class Rating(userId: Int, movieId: Int, rating: Double)
 
   def main(args: Array[String]): Unit = {
+    import org.apache.spark
 
     Frame.logger
-    val sc = Frame.contextSpark("MovieRecommendationsALS")
+    val spark = SparkSession
+      .builder()
+      .appName("ALS Sample")
+      .master("local[*]")
+      .getOrCreate()
+
+    import spark.implicits._
+
     val nameDict = loadMovies
 
-    val data = sc.textFile("../ml-100k/u.data")
-    val ratings = data.map( x => x.split('\t') ).map( x => Rating(x(0).toInt, x(1).toInt, x(2).toDouble) ).cache()
-
-    val als = new ALS()
+    val data = spark.read.textFile("../ml-100k/u.data")
+    val ratings = data.map(x => x.split('\t')).map(x => Rating(x(0).toInt, x(1).toInt, x(2).toDouble)).toDF()
 
     println("\nTraining recommendation model...")
+
+    val als = new ALS()
+      .setMaxIter(5)
+      .setRegParam(0.01)
+      .setUserCol("userId")
+      .setItemCol("movieId")
+      .setRatingCol("rating")
+
+    val model = als.fit(ratings)
+
+    val userid: Int = args(0).toInt
+    val users = Seq(userid).toDF("userId")
+    val recommendations = model.recommendForAllUsers(10)
+
+    println("\nTop 10 recommendations for user" + userid + ":")
+
+
 
   }
 }
